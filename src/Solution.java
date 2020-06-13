1d2a3ffce5b7cad9801b20a89d11374efd3c6760
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-06-13 18:23:03 +0800

    70

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,18 @@
-public class Solution {
+class Solution {
+    /**
+     * f(n) = f(n-1) + f(n-2)
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int fn1 = 1, fn2 = 1;
+        int fn = 1;
 
-
-
+        for (int i=1; i<n; i++) {
+            fn = fn1 + fn2;
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        return fn;
+    }
 }
