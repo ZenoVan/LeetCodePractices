e8ddca7a31916cf3251cbfe0d89d3822efbe8b65
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-06-14 18:59:07 +0800

    1128

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println("Hello World!");
+        System.out.println();
 
     }
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,22 @@
 class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] nums = new int[100];
 
+        for (int i = 0; i<dominoes.length; i++) {
+            if (dominoes[i][0] > dominoes[i][1]) {
+                int temp = dominoes[i][0];
+                dominoes[i][0] = dominoes[i][1];
+                dominoes[i][1] = temp;
+            }
+            nums[dominoes[i][0]*10 + dominoes[i][1]]++;
+        }
 
+        int count = 0;
+        for (int i = 0; i<nums.length; i++) {
+            if (nums[i] != 0 && nums[i] != 1)
+                count += (nums[i]*(nums[i]-1))/2;
+        }
 
+        return count;
+    }
 }
