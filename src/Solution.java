e8abb3d1f20b0af600d5c3da2744fa1c6f195fe4
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-06-15 20:12:21 +0800

    14

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,21 @@
 class Solution {
-
-
-
+    public String longestCommonPrefix(String[] strs) {
+        String res = "";
+        if (strs.length > 0)
+            res = strs[0];
+        for (String str : strs) {
+            if (res.equals(""))
+                break;
+            for (int i = 0; i<res.length(); i++) {
+                if (i == str.length()) {
+                    res = str;
+                    break;
+                }
+                if (res.charAt(i) != str.charAt(i)) {
+                    res = res.substring(0, i);
+                }
+            }
+        }
+        return res;
+    }
 }
